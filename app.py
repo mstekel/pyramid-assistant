8296815267d6da2ce4a8329aef2a60f7e3f43375
moshe.stekel <moshe.stekel@pyramidanalytics.com>
2023-04-18 19:31:43 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    # Extract intent and parameters from the request
+    intent = req['queryResult']['intent']['displayName']
+    params = req['queryResult']['parameters']
+
+    # Generate response based on the intent
+    if intent == 'greet':
+        name = params['name'] if 'name' in params else 'there'
+        response = f'Hello, {name}!'
+    elif intent == 'goodbye':
+        response = 'Goodbye, have a nice day!'
+    else:
+        response = 'I am not sure what you mean.'
+
+    # Create response payload and return it
+    payload = {
+        'fulfillmentText': response
+    }
+    return jsonify(payload)
+
+if __name__ == '__main__':
+    app.run(debug=True)
