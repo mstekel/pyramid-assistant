348bf8828e851d48499ee153b23ac149d59210ef
moshe.stekel <moshe.stekel@pyramidanalytics.com>
2023-04-19 17:27:48 +0300

    Fix the default answer

--- app.py
+++ app.py
@@ -4,25 +4,22 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
-
-    # Extract intent and parameters from the request
-    intent = req['queryResult']['intent']['displayName']
-    params = req['queryResult']['parameters']
-
-    # Generate response based on the intent
-    if intent == 'greet':
-        name = params['name'] if 'name' in params else 'there'
-        response = f'Hello, {name}!'
-    elif intent == 'goodbye':
-        response = 'Goodbye, have a nice day!'
-    else:
-        response = 'I am Pyramid. Your intent was ' + intent
 
     # Create response payload and return it
     payload = {
-        'fulfillmentText': response
+        "candidates": [
+            {
+                "first_simple": {
+                    "variants": [
+                        {
+                          "speech": "Moshe Moshe"
+                        }
+                    ]
+                }
+            }
+        ]
     }
+
     return jsonify(payload)
 
 if __name__ == '__main__':
