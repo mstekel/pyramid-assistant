b9297e7b09f369b5d0210536ad498e595ce9a9c0
moshe.stekel <moshe.stekel@pyramidanalytics.com>
2023-04-19 19:06:00 +0300

    Fix packages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from google.protobuf.json_format import MessageToJson
-from google.cloud.dialogflowcx_v3.types import webhook
+from google.cloud.dialogflowcx_v3.types import WebhookRequest, WebhookResponse, Intent, ResponseMessage
 
 app = Flask(__name__)
 
@@ -8,12 +8,12 @@ app = Flask(__name__)
 def webhook():
     # Convert the request to JSON format
     request_json = request.get_json(silent=True, force=True)
-    request_proto = webhook.WebhookRequest()
-    webhook.JsonFormat.Parse(request_json, request_proto)
+    request_proto = WebhookRequest()
+    WebhookRequest.FromJsonString(request_json, request_proto)
 
     # Parse the JSON request to extract the intent and parameters
-    intent = request_proto.intent_info.display_name
-    parameters = request_proto.parameters
+    intent = request_proto.intent.display_name
+    parameters = request_proto.parameters.fields
 
     # Handle the intent and generate a response
     if intent == 'Default Welcome Intent':
@@ -22,8 +22,9 @@ def webhook():
         response_text = 'I don\'t understand that command.'
 
     # Create a response object and set the text response
-    response = webhook.WebhookResponse()
-    response.fulfillment_response.messages.add(text=webhook.Intent.Message(text= [response_text]))
+    response = WebhookResponse()
+    text_response = ResponseMessage(text=Intent.Message.Text(text=[response_text]))
+    response.payload = {"response_messages": [text_response]}
 
     # Convert the response object to JSON format
     response_json = MessageToJson(response)
